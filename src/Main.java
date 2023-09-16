3f2507cf10d19da6786f5e1e1d0a11ac2ded813a
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-09-16 13:25:08 -0300

    exercicio-completo

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Para iniciar a calculadora PA/PG Digite 2 numeros separados por virgula: Valor inicial e valor para raiz. Exemplo: 5 , 2");
-        String entrada = sc.nextLine();
-        String[] valores = entrada.split(",");
-        if (valores.length == 2){
-            int valorInicial = Integer.parseInt(valores[0].trim());
-            int valorRaiz = Integer.parseInt(valores[1].trim());
-            System.out.printf(" Você digitou %d para valor inicial e %d para valor da raiz.", valorInicial, valorRaiz);
+        System.out.println("Para iniciar a calculadora PA/PG Digite o Valor inicial ");
+        int valorInicial = sc.nextInt();
+        System.out.println("Agora escolha um valor para a raiz");
+        int valorRaiz = sc.nextInt();
+        System.out.println("Vamos iniciar a calculadora de PA ou de PG? - Digite 1 para PA e 2 para PG:");
+        int opcao = sc.nextInt();
+        if (opcao == 1){
+        System.out.println("Quer imprimir os Dez primeiros números da PA? Digite 1- SIM ou 2 - NÃo.");
+        int opcao = sc.nextInt();
+            if(opcao == 1){
+                System.out.println("Os 10 primeiros números da PA são: ");
+                for (int i = 0; i < 10; i++) {
+                valorInicial += valorRaiz;
+                }
+            }else if(opcao==2){
+                System.out.println("Programa encerrado, volte sempre.");
+            }else {
+            System.out.println("Programa encerrado, volte sempre.");
+            }
+        }else if (opcao == 2){
+        System.out.println("Quer imprimir os Dez primeiros números da PG? Digite 1- SIM ou 2 - NÃo.");
+        int opcao = sc.nextInt();
+            if(opcao == 1){
+                System.out.println("Os 10 primeiros números da PG são: ");
+                for (int i = 0; i < 10; i++) {
+                valorInicial *= valorRaiz;
+                }
+            }else if(opcao==2){
+            System.out.println("Programa encerrado, volte sempre.");
+            }else {
+            System.out.println("Programa encerrado, volte sempre.");
         }
     }
 }
